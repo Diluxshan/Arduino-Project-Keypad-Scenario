79fb4c4e4e62f69db0f996bd9c002c4d0ddb0e09
Diluxshan <diluxv@gmail.com>
2018-09-02 18:25:38 +0530

    arduino file will be ready to upload

---
+++ keypad_with_password.ino
@@ -0,0 +1,88 @@
+#include <Key.h>
+#include <Keypad.h>
+
+#include <Keypad.h>
+#include <Password.h>
+ 
+String newPasswordString; //hold the new password
+char newPassword[6]; //charater string of newPasswordString
+ 
+//initialize password to 1234
+//you can use password.set(newPassword) to overwrite it
+Password password = Password( "1234" );
+ 
+byte maxPasswordLength = 7; 
+byte currentPasswordLength = 0;
+const byte ROWS = 4; // Four rows
+const byte COLS = 4; // Four columns
+ 
+//Define the keymap
+char keys[ROWS][COLS] = {
+{'D','#','0','*'},
+{'C','9','8','7'},
+{'B','6','5','4'},
+{'A','3','2','1'}
+};
+ 
+//// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte rowPins[ROWS] = {6,7,8,9}; //connect to row pinouts
+ 
+// Connect keypad COL0, COL1, COL2 and COL3 to these Arduino pins.
+byte colPins[COLS] = {2,3,4,5}; //connect to column pinouts
+ 
+// Create the Keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+ 
+void setup(){
+   Serial.begin(9600);
+}
+ 
+void loop(){
+   char key = keypad.getKey();
+   if (key != NO_KEY){
+      delay(60); 
+      switch (key){
+      case 'A': break; 
+      case 'B': break; 
+      case 'C': break; 
+      case 'D': changePassword(); break; 
+      case '#': checkPassword(); break;
+      case '*': resetPassword(); break;
+      default: processNumberKey(key);
+      }
+   }
+}
+ 
+void processNumberKey(char key) {
+   Serial.print(key);
+   currentPasswordLength++;
+   password.append(key);
+   if (currentPasswordLength == maxPasswordLength) {
+      checkPassword();
+   } 
+}
+
+void checkPassword() {
+   if (password.evaluate()){
+      Serial.println(" OK.");
+   } else {
+      Serial.println(" Wrong passwowrd!");
+   } 
+   resetPassword();
+}
+
+void resetPassword() {
+   password.reset(); 
+   currentPasswordLength = 0; 
+}
+
+void changePassword() {
+   newPasswordString = "1234";
+   newPasswordString.toCharArray(newPassword, newPasswordString.length()+1); //convert string to char array
+password.set(newPassword);
+   resetPassword();
+   Serial.print("Password changed to ");
+   Serial.println(newPasswordString);
+}
+ 
+ 
